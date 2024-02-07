24fe6efb081f1ffa5def14b9a6be9b0fb3e0d5b2
hariharn12 <140273143+hariharn12@users.noreply.github.com>
2024-02-07 10:35:07 +0530

    create a reactive forms and adding validation errors

--- reactive and template forms/src/app/app.component.ts
+++ reactive and template forms/src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
